1ffa1585e29e4e6552984491671acda2fe01c5b1
André Cerqueira <78262628+AndreCerqueira@users.noreply.github.com>
2022-06-14 10:37:28 +0100

    flutter tab separator

---
+++ js-scripts/utils.js
@@ -0,0 +1,34 @@
+function copyToClipBoard() {
+    /* Get the text field */
+    var copyText = document.getElementById("textAreaResult");
+
+    /* Select the text field */
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+    /* Copy the text inside the text field */
+    navigator.clipboard.writeText(copyText.value);
+}
+
+function changeModelType() {
+    selectModelType = document.getElementById("selectModelType")
+    selectedOption = selectModelType.value
+
+    if (selectedOption == "kotlin") {
+        document.getElementById("inputPackage").style.display = 'block';
+    }
+    else {
+        document.getElementById("inputPackage").style.display = 'none';
+    }
+}
+
+function openTab(evt) {
+    var i, tablinks;
+
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    
+    evt.currentTarget.className += " active";
+}
--- py-scripts/convertJsonToModel.py
+++ py-scripts/convertJsonToModel.py
@@ -1,75 +1,106 @@
 from js import document
+from pyodide import create_proxy
 import json
-
-'''
-primeiro procurar por listas.
-
-para cada lista cria uma classe para ela em cima
-
-
-modificar from json com 
-
-List<Certificates> _certificates = List<Certificates>.from(
-        certificatesJsonArray
-            .map((json) => Certificates.fromJson(json))
-            .toList());
-
-e 
-
-certificates: _certificates,
-'''
 
 def convertJson(event):
     textAreaResult = document.getElementById('textAreaResult')
-    selectModelType = document.getElementById("selectModelType")
-    selectedOption = selectModelType.value
-
+    selectedOption = document.getElementById("selectModelType").value
     result = ""
 
+    # Get Selected Tab
+    selectedTab = getSelectedTab()
+
     if (selectedOption == "flutter"):
-        result = convertToFlutter()
+        result = convertToFlutter(selectedTab)
     elif (selectedOption == "kotlin"):
         result = convertToKotlin()
 
     textAreaResult.value = result
 
 
-def convertToFlutter():
+def convertToFlutter(selectedTab):
 
     className = document.getElementById("inputClassName").value
     jsonStr = document.getElementById('insertJsonArea').value
     jsonStr = jsonStr.replace("\n", "")
     jsonStr = jsonStr.replace("\t", "")
+    document.querySelector("#tabMenu").innerHTML = ""; 
     data = json.loads(jsonStr)
+    fullCode = ""
+    html = ""
+    htmlList = ""
+    tabItems = []
 
-    result = "class " + className + " {\n\t"
+    # Create Tabs
+    for item in data:
+        if (getItemType(data[item]) == "list"):
+            htmlList += "<button id=\"tab-" + item + "\" class=\"tablinks\" onclick=\"openTab(event)\">" + item + "</button>"
+            tabItems.append({"name": item, "body":data[item][0]})
+
+    if (htmlList == ""):
+        html += "<button id=\"tab-" + className + "\" class=\"tablinks\" onclick=\"openTab(event)\">" + className + "</button>"
+
+        document.querySelector("#tabMenu").innerHTML += html; 
+        addTabListener(className)
+    
+    # List Code Needed
+    else:
+        html += "<button id=\"tab-all\" class=\"tablinks\" onclick=\"openTab(event)\">All</button>"
+        html += "<button id=\"tab-" + className + "\" class=\"tablinks\" onclick=\"openTab(event)\">" + className + "</button>"
+        html += htmlList
+
+        document.querySelector("#tabMenu").innerHTML += html; 
+
+        addTabListener("all")
+        addTabListener(className)
+
+        # Generate List Code for All Tab
+        for item in tabItems:
+            addTabListener(item["name"])
+            fullCode += generateFlutterModelCode(item["name"], item["body"])
+
+    # Generate Base Code
+    if (selectedTab == className):
+        fullCode = generateFlutterModelCode(className, data)
+    elif (selectedTab == "all"):
+        fullCode += generateFlutterModelCode(className, data)
+    else:
+        fullCode = generateFlutterModelCode(selectedTab, data[selectedTab][0])
+
+    SelectTab(selectedTab)
+    return fullCode
+
+
+def generateFlutterModelCode(className, data):
+
+    model_code = "class " + className + " {\n\t"
     
     for item in data:
-        result += "final " + getItemTypeDart(data[item]) +" " + item + ";\n\t"
+        model_code += "final " + getItemType(data[item]) +" " + item + ";\n\t"
 
-    result += "\n\tconst " + className + "({"
+    model_code += "\n\tconst " + className + "({"
     
     for item in data:
-        result += "\n\t\trequired this." + item + ","
+        model_code += "\n\t\trequired this." + item + ","
 
-    result += "\n\t});\n\n\t"
-    result += "factory " + className + ".fromJson(Map<String, dynamic> json) {\n\t\t"
-    result += "return " + className + "("
+    model_code += "\n\t});\n\n\t"
+    model_code += "factory " + className + ".fromJson(Map<String, dynamic> json) {\n\t\t"
+    model_code += "return " + className + "("
     
     for item in data:
-        result += "\n\t\t\t" + item + ": json['" + item + "'],"
+        model_code += "\n\t\t\t" + item + ": json['" + item + "'],"
     
-    result += "\n\t\t);\n\t"
-    result += "}\n\n\t"
-    result += "Map<String, dynamic> toJson() => {"
+    model_code += "\n\t\t);\n\t"
+    model_code += "}\n\n\t"
+    model_code += "Map<String, dynamic> toJson() => {"
     
     for item in data:
-        result += "\n\t\t'" + item + "': " + item + ","
+        model_code += "\n\t\t'" + item + "': " + item + ","
 
-    result += "\n\t};\n"
-    result += "}"
+    model_code += "\n\t};\n"
+    model_code += "}\n\n"
 
-    return result
+    return model_code
 
 
 def convertToKotlin():
@@ -85,14 +116,14 @@ def convertToKotlin():
     result += "class " + className + " {\n\t"
 
     for item in data:
-        result += "\n\tvar " + item + ": " + getItemTypeKotlin(data[item]) + "? = null"
+        result += "\n\tvar " + item + ": " + getItemType(data[item]) + "? = null"
 
     result += "\n\n\tconstructor(){\n\t"
     result += "}\n\n\t"
     result += "constructor(\t\t"
 
     for item in data:
-        result += "\n\t\t" + item + ": " + getItemTypeKotlin(data[item]) + ","
+        result += "\n\t\t" + item + ": " + getItemType(data[item]) + ","
 
     result += "\n\t){\t\t"
 
@@ -105,7 +136,7 @@ def convertToKotlin():
     result += "val " + str("_" + className).lower() + " = " + className + "()\n\n\t\t\t"
 
     for item in data:
-        result += "" + str("_" + className).lower() + "." + item + " = if (!jsonObject.isNull(\"" + item + "\")) jsonObject.get" + getItemTypeKotlin(data[item]).capitalize() + "(\"" + item + "\") else null\n\t\t\t"
+        result += "" + str("_" + className).lower() + "." + item + " = if (!jsonObject.isNull(\"" + item + "\")) jsonObject.get" + getItemType(data[item]).capitalize() + "(\"" + item + "\") else null\n\t\t\t"
     
     result += "\n\t\t\treturn " + str("_" + className).lower() + "\n\t\t"
     result += "}\n\t"
@@ -115,7 +146,7 @@ def convertToKotlin():
     return result
 
 
-def getItemTypeDart(item):
+def getItemType(item):
 
     itemType = type(item)
     itemType = str(itemType)
@@ -131,17 +162,27 @@ def getItemTypeDart(item):
     return itemType
 
 
-def getItemTypeKotlin(item):
+def addTabListener(tabName):
+    convertButton = document.getElementById("tab-" + tabName)
+    cc = create_proxy(convertJson)
+    convertButton.addEventListener("click", cc)
 
-    itemType = type(item)
-    itemType = str(itemType)
-    itemType = itemType.replace("<", "")
-    itemType = itemType.replace(">", "")
-    itemType = itemType.replace("'", "")
-    itemType = itemType.replace(" ", "")
-    itemType = itemType.replace("class", "")
+
+def getSelectedTab():
+    tablinks = document.getElementsByClassName("tablinks")
+    selectedTab = "all"
+    for tab in tablinks:
+        if ("active" in tab.className):
+            selectedTab = tab.id
+            selectedTab = selectedTab.replace("selected", "")
+            selectedTab = selectedTab.replace(" ", "")
+            selectedTab = selectedTab.replace("tab-", "")
     
-    if (itemType == "str"):
-        itemType = "String"
+    return selectedTab
 
-    return itemType
+
+def SelectTab(tabName):
+    tablinks = document.getElementsByClassName("tablinks")
+    for tab in tablinks:
+        if (tab.id == "tab-" + tabName):
+            document.getElementById(tab.id).className += " active"
